b260dc5fe09a490e819e4093a9996b7ca219df01
HensalDeon <hensaldeon777@gmail.com>
2024-10-13 22:44:59 +0530

    updated validations color etc

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,72 +6,61 @@ import { slideIn } from "../utils/motion";
 import { contactInputs } from "../constants";
 import useContact from "../hooks/useContact";
 const Contact = () => {
-  const { formik, loading } = useContact();
-  return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-    >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+    const { formik, loading } = useContact();
+    return (
+        <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+            <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          onSubmit={formik.handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          {contactInputs.map((item, index) => (
-            <div key={index}>
-              <label className="flex flex-col relative">
-                <span className="text-white font-medium mb-4">
-                  Your {item?.label}
-                </span>
-                {item.type == "textarea" ? (
-                  <textarea
-                    id={item?.name}
-                    name={item?.name}
-                    rows="7"
-                    placeholder={item?.label}
-                    value={formik.values[item.name]}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-                  />
-                ) : (
-                  <input
-                    name={item?.name}
-                    type={item?.type}
-                    placeholder={item?.label}
-                    className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                )}
-                {formik.touched[item.name] && formik.errors[item.name] ? (
-                  <span className="has-error">{formik.errors[item.name]}</span>
-                ) : null}
-              </label>
-            </div>
-          ))}
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
+                <form onSubmit={formik.handleSubmit} className="mt-12 flex flex-col gap-8">
+                    {contactInputs.map((item, index) => (
+                        <div key={index}>
+                            <label className="flex flex-col relative">
+                                <span className="text-white font-medium mb-4">Your {item?.label}</span>
+                                {item.type == "textarea" ? (
+                                    <textarea
+                                        id={item?.name}
+                                        name={item?.name}
+                                        rows="7"
+                                        placeholder={item?.label}
+                                        value={formik.values[item.name]}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                                    />
+                                ) : (
+                                    <input
+                                        name={item?.name}
+                                        type={item?.type}
+                                        placeholder={item?.label}
+                                        className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values[item.name]}
+                                    />
+                                )}
+                                {formik.touched[item.name] && formik.errors[item.name] ? (
+                                    <span className="has-error">{formik.errors[item.name]}</span>
+                                ) : null}
+                            </label>
+                        </div>
+                    ))}
+                    <button
+                        type="submit"
+                        className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+                        disabled={loading}
+                    >
+                        {loading ? "Sending..." : "Send"}
+                    </button>
+                </form>
+            </motion.div>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-      >
-        <EarthCanvas />
-      </motion.div>
-    </div>
-  );
+            <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+                <EarthCanvas />
+            </motion.div>
+        </div>
+    );
 };
 
 const ContactSection = SectionWrapper(Contact, "contact");
--- src/hooks/useContact.js
+++ src/hooks/useContact.js
@@ -2,44 +2,59 @@ import { useFormik } from "formik";
 import emailjs from "@emailjs/browser";
 import { useState } from "react";
 import { initialValues, validationSchema } from "../utils/validation";
+import toast from "react-hot-toast";
 
 const useContact = () => {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const formik = useFormik({
-    initialValues: initialValues,
-    validationSchema: validationSchema,
-    validateOnBlur: true,
-    validateOnChange: true,
-    onSubmit: async (values, { resetForm }) => {
-      emailjs
-        .send(
-          import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-          import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-          {
-            from_name: values.name,
-            to_name: "JavaScript Mastery",
-            from_email: values.email,
-            to_email: "hensal@jsmastery.pro",
-            message: values.message,
-          },
-          import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-        )
-        .then(
-          () => {
-            setLoading(false);
-            resetForm();
-            alert("Thank you. I will get back to you as soon as possible.");
-          },
-          (error) => {
-            setLoading(false);
-            console.error(error);
-            alert("Ahh, something went wrong. Please try again.");
-          }
-        );
-    },
-  });
-  return { formik, loading };
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: validationSchema,
+        validateOnBlur: true,
+        validateOnChange: true,
+        onSubmit: async (values, { resetForm }) => {
+            setLoading(true);
+            emailjs
+                .send(
+                    import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+                    import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+                    {
+                        from_name: values.name,
+                        to_name: "Hensal Deon",
+                        from_email: values.email,
+                        message: values.message,
+                    },
+                    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+                )
+                .then(
+                    () => {
+                        setLoading(false);
+                        resetForm();
+                        toast("Thank you. I will get back to you as soon as possible.", {
+                            icon: "👏",
+                            style: {
+                                borderRadius: "10px",
+                                background: "#100d25",
+                                color: "#fff",
+                            },
+                        });
+                    },
+                    (error) => {
+                        setLoading(false);
+                        console.error(error);
+                        toast("Ahh, something went wrong. Please try again later.", {
+                            icon: "☹️",
+                            style: {
+                                borderRadius: "10px",
+                                background: "#100d25",
+                                color: "#fff",
+                            },
+                        });
+                    }
+                );
+        },
+    });
+    return { formik, loading };
 };
 
 export default useContact;
