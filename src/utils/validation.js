69c6132856bc778997b15f1e977f7ea59297995e
HensalDeon <hensaldeon777@gmail.com>
2024-10-13 22:18:42 +0530

    updated validation message

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,5 +9,5 @@ export const initialValues = {
 export const validationSchema = Yup.object().shape({
     name: Yup.string().trim().required("name is required"),
     email: Yup.string().trim().email().required("email is required"),
-    message: Yup.string().trim().email().required("message is required"),
+    message: Yup.string().trim().required("message is required"),
 });
